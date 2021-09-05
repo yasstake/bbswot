acd72a1dc625b03995869ce0de17ed250d903144
yass <yass@takibi.net>
2021-09-05 14:45:48 +0900

    CHANGE: Change Liquid pool loop time 30->20

--- docker/logging_container/rundaemon.sh
+++ docker/logging_container/rundaemon.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-docker rm -f BBP-DAEMON-A
-docker run --name BBP-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BBP -flag_file  /bitlog/BBPFLAG -exit_wait 240
+docker rm -f BB-DAEMON-A
+docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
 sleep 240
 
 
-docker rm -f BBP-DAEMON-B
-docker run --name BBP-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BBP -flag_file  /bitlog/BBPFLAG -exit_wait 240
+docker rm -f BB-DAEMON-B
+docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
+
 
 
 
