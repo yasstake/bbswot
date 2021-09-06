495d86da268cf639a83606d3de817ad953ba67e2
yass <yass@takibi.net>
2021-09-07 08:40:51 +0900

    Add: Extract log to csv file

--- cmd/extractwslog/extractwslog.go
+++ cmd/extractwslog/extractwslog.go
@@ -13,7 +13,7 @@ func extractSingleFile(f string) {
 	archiveLogMode := bb.CheckArchiveLog(f)
 
 	if archiveLogMode {
-		log.Println("Unsupported archive log")
+		extractArchiveLog(f)
 		return
 	}
 
@@ -25,12 +25,53 @@ func extractSingleFile(f string) {
 		rec := stream.Text()
 
 		r1, r2, r3, r4, r5 := bb.ParseWsLogRec(rec)
-		fmt.Printf("%d,%d,%F,%F,%s\n", r1, r2, r3, r4, r5)
+
+		sec := int64(r2 / 1_000_000)
+		msec := int64((r2 % 1_000_000) / 1000)
+
+		action := ""
+
+		if r1 == common.TRADE_BUY {
+			action = common.TRADE_BUY_STR
+		} else if r1 == common.TRADE_SELL {
+			action = common.TRADE_SELL_STR
+		}
+
+		size := int64(r4)
+		if action != "" {
+			fmt.Printf("%d.%d,BTCUSD,%s,%d,%.0f,%s\n", sec, msec, action, size, r3, r5)
+		}
 
 		// TODO: for debug perpose [after debugging below lines shold be removed.]
 		if 60_000 < r3 {
 			fmt.Println("HIT")
 		}
+
+		recordNumber += 1
+	}
+}
+
+func extractArchiveLog(file string) {
+	stream := common.OpenFileReader(file)
+
+	stream.Scan() // skip header line
+
+	var recordNumber int64
+	for stream.Scan() {
+		rAction, rTimeMs, rPrice, rVolume, rOption := bb.ParseArchivedLogRec(stream.Text())
+
+		action := ""
+
+		sec := int64(rTimeMs / 1_000_000)
+		msec := int64((rTimeMs % 1_000_000) / 1000)
+
+		if rAction == common.TRADE_BUY {
+			action = common.TRADE_BUY_STR
+		} else if rAction == common.TRADE_SELL {
+			action = common.TRADE_SELL_STR
+		}
+
+		fmt.Printf("%d.%d,BTCUSD,%s,%d,%.0f,%s\n", sec, msec, action, int(rVolume), rPrice, rOption)
 
 		recordNumber += 1
 	}
--- flux/select_ohlc.flux
+++ flux/select_ohlc.flux
@@ -1,16 +1,30 @@
+// TODO: NOT IMPLEMENTED
+// Plan: make Open/High/Low/Close/Volume separately, and merge into ohlcv
 
 
-agg =
+from(bucket: "btc")
+  |> range(start: 1970-01-01T00:00:00Z)
+  |> filter(fn: (r) => r["_measurement"] == "exec")
+  |> filter(fn: (r) => r["_field"] == "price" or r["_field"] == "size" or r["_field"]=="side")
+  |> drop(columns: ["_start", "_stop", "_measurement"])
+  |> pivot(rowKey: ["_time"], columnKey:["_field"], valueColumn: "_value")
+  |> aggregateWindow(every: 10m, fn: last)
 
-open =
+//agg =
+
+//open =
 
 
-high =
+//high =
 
 
-close =
+//close =
 
 
-close =
+//volume =
+
+
+//ohlcv =
+
 
 
