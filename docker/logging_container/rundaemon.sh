be46827fcdf832fe6caa40b34d701629aee5a9cd
yass <yass@takibi.net>
2021-09-24 21:10:00 +0900

    Change: Obsolete Liquid RestAPI -> change WS API

--- docker/logging_container/rundaemon.sh
+++ docker/logging_container/rundaemon.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 docker rm -f BB-DAEMON-A
-docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
+docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
 sleep 240
 
 
 docker rm -f BB-DAEMON-B
-docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bbplogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
+docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
 
 
 
